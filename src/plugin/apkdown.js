688cabcc1a91ab39f5dead04fd6bcdc64c669cd1
STAR-KING0 <kingdavid23565@gmail.com>
2024-07-11 13:41:55 +0100

    Update apkdown.js

--- src/plugin/apkdown.js
+++ src/plugin/apkdown.js
@@ -73,7 +73,7 @@ const searchAPK = async (m, Matrix) => {
                 text: "© Powered By Arthur-MD"
               }),
               header: proto.Message.InteractiveMessage.Header.create({
-                ...(await prepareWAMessageMedia({ image: { url: `https://telegra.ph/file/fbbe1744668b44637c21a.jpg` } }, { upload: Matrix.waUploadToServer })),
+                ...(await prepareWAMessageMedia({ image: { url: `https://telegra.ph/file/e387804fa0e8e7fd23b27.jpg` } }, { upload: Matrix.waUploadToServer })),
                 title: ``,
                 gifPlayback: true,
                 subtitle: "",
